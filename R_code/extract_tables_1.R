df3705c6a6ec5a77fd1c3ee8f1f679f954cba7b6
TTang_20 <yiyit62@gmail.com>
2017-07-21 15:40:11 -0500

    Create one table with all measurement
    Fix more cast id problem for OSD data file.

--- R_code/extract_tables_1.R
+++ R_code/extract_tables_1.R
@@ -12,7 +12,7 @@ library(dplyr)
 
 #Make table with cast location and time information called loc_date
 
-char_lines=readLines("ocldb1499972358.22646.OSD.csv")
+char_lines=readLines("ocldb1499972358.22646.MRB.csv")
 
 lat<-str_match(char_lines,"(Latitude)\\s+,,\\s*(\\d+.\\d*)")
 lon<-str_match(char_lines,"(Longitude)\\s+,,\\s*(-\\d+.\\d*)")
@@ -20,8 +20,6 @@ year<-str_match(char_lines,"(Year)\\s+,,\\s*(\\d*)")
 month<-str_match(char_lines,"(Month)\\s+,,\\s*(\\d*)")
 day<-str_match(char_lines,"(Day)\\s+,,\\s*(\\d*)")
 cast<-str_match(char_lines,"(CAST)\\s+,,\\s*(\\d*)")
-beg<-str_match(char_lines,"VARIABLES ,")
-end<-str_match(char_lines,"END OF VARIABLES SECTION")
 # Save the latitude data in a matrix with col one lat and col 2 the value of the lat
 lat_df<- data.frame(na.omit(lat[,-1:-2]),stringsAsFactors=FALSE)
 lon_df<- data.frame(na.omit(lon[,-1:-2]),stringsAsFactors=FALSE)
@@ -29,8 +27,6 @@ year_df<-data.frame(na.omit(year[,-1:-2]),stringsAsFactors=FALSE)
 month_df<-data.frame(na.omit(month[,-1:-2]),stringsAsFactors=FALSE)
 day_df<-data.frame(na.omit(day[,-1:-2]),stringsAsFactors=FALSE)
 cast_df<-data.frame(na.omit(cast[,-1:-2]),stringsAsFactors=FALSE)
-beg_df<-data.frame(na.omit(beg),stringsAsFactors=FALSE)
-end_df<-data.frame(na.omit(end),stringsAsFactors=FALSE)
 
 colnames(lat_df)<-c("lat")
 colnames(lon_df)<-c("lon")
@@ -38,8 +34,15 @@ colnames(year_df)<-c("year")
 colnames(month_df)<-c("month")
 colnames(day_df)<-c("day")
 colnames(cast_df)<-c("cast")
-colnames(beg_df)<-c("beg")
-colnames(end_df)<-c("end")
+##--------------Alternative way to find beg and end lines---------------
+#beg<-str_match(char_lines,"VARIABLES ,")
+#end<-str_match(char_lines,"END OF VARIABLES SECTION")
+
+#beg_df<-data.frame(na.omit(beg),stringsAsFactors=FALSE)
+#end_df<-data.frame(na.omit(end),stringsAsFactors=FALSE)
+
+#colnames(beg_df)<-c("beg")
+#colnames(end_df)<-c("end")
 
 # Combine the cast, location and date data into one table
 loc_date<-cbind(cast_df,lat_df,lon_df,year_df,month_df,day_df)
@@ -48,10 +51,9 @@ head(loc_date)
 
 #Extract beginning and ending lines for each table
 
-beg=which(str_detect(char_lines, "VARIABLES"))
-end=which(str_detect(char_lines, "END*"))
-
-L<-length(beg)
+beg=which(str_detect(char_lines, "^VARIABLES*"))
+end=which(str_detect(char_lines, "END OF VARIABLES SECTION"))
+endBio<-which(str_detect(char_lines, "END OF BIOLOGY SECTION"))
 
 ########################################################################
 
@@ -62,26 +64,29 @@ L<-length(beg)
 #Set the number of cores to be used (parallel processing)
 mc <- getOption("mc.cores", 10)
 
-#Make a list to store the output temporarily (great for debugging)
-table_list_CSV=vector("list", length(beg)) 
 
-#Make a function to read in using data.tables() in case there is an error in fread()
-alternative_read=function(i,col){
-  alt_data=read.table("ocldb1499972358.22646.MRB.csv",skip=beg[i]+2,
-                      nrows=end[i]-beg[i]-3,sep=',',header=FALSE)
-  alt_data=alt_data[,col]
-  return(alt_data)
-}
 #initilize the table to store all measurements
 measure<- data.frame(flag=integer(),
                      Depth=character(), 
                      Temperatur=character(),
                      Cast=integer(),
                      stringsAsFactors=FALSE)
+L<-length(beg)
+casts<-vector()
+casts[1]<-cast_df$cast[1]
+casts[2]<-cast_df$cast[2]
+for (i in (3:L)){
+  
+  casts[i]=max(which(str_detect(char_lines[(beg[i]-50):(beg[i])], "^CAST*")))+beg[i]-51
+  castcatch<-str_match(char_lines[as.numeric(casts[i])],"(CAST)\\s+,,\\s*(\\d*)")
+  casts[i]<-as.numeric(castcatch[,3])
+  
+}
+
 #Record system time
 t = Sys.time()
-Seq<-1:1:L
- 
+Seq<-1:L
+
 for (i in Seq) {
   print(i)
   #Figure out how many columns
@@ -108,7 +113,7 @@ for (i in Seq) {
     castid<-max(which(str_detect(char_lines[(begind[i]-50):(begind[i])], "CAST")))
   }
   #Adds the cast number
-  data$Cast=rep(str_extract(char_lines[castsid],"[0-9]+"),times=nrow(data))
+  data$Cast=rep(casts[i],times=nrow(data))
   
   #Adds header names
   names(data)=c("flag",header,"Cast")
