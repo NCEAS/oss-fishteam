2987b2cee7c27160acf3674229cd753e0c79da7a
OSS2017 <ilich@aurora>
2017-07-24 17:11:01 -0700

    Improved length histogram, Calculated Gear frequency, and Von Bert Growth

--- Length_Distribution.R
+++ Length_Distribution.R
@@ -1,6 +1,7 @@
 #Using datatables GLFREC and INVREC, determine what the length frequency of red snapper is caught with gear (GEAR_TYPE) shrimp trawl (ST)
-
 #Not looking at sex of fish because "male and female red snapper grow rapidly and at about the same rate until about 8 years old and about 28 inches in length."- Seagrant 
+
+rm(list=ls()) #clear workspace
 library(tidyverse)
 setwd("~/oss/Synthesis")#Merge the tables
 INVREC<- read.table("Seamap/INVREC.txt", sep=",", stringsAsFactors = FALSE, header = TRUE)
@@ -16,15 +17,50 @@ length_freq<- left_join(select(GLF, CRUISEID, STATIONID, LEN_GLF, MEASCD_GLF), s
 
 length(length_freq$STATIONID)==length(GLF$STATIONID) #New size matches GLF so not repating length values. Merge successful.
 
-#Frequency of red snapper length
-length_freq_gs40_ms163<-length_freq %>% filter(GEAR_SIZE==40, MESH_SIZE==1.63) #Only looking at Shrimp trawl, GEAR_SIZE=40 and MESH_SIZE=1.63
-hist(length_freq_gs40_ms163$LEN_GLF, xlab = "Length (mm)", main = "Red Snapper Length Distribution")
+#Check freqency of each gear type
+length_freq<- length_freq %>% mutate(GEAR_MERGE=paste(GEAR_TYPE,GEAR_SIZE,MESH_SIZE, sep="_"))
+Gear_Counts<- length_freq %>% count(GEAR_MERGE)
+Percent_ST401.63<- ((filter(Gear_Counts, GEAR_MERGE=="ST_40_1.63") %>% select(n))/length(length_freq$STATIONID)*100)[[1]]
+      # 91.78001     Almost 92% of red snapper catches are with Shrimp trawl of size 40 and mesh size 1.63
+
+#Extract most common gear type and remove ones that are size of NA
+length_freq_gs40_ms163<-length_freq %>% filter(GEAR_SIZE==40, MESH_SIZE==1.63) %>% filter(!is.na(LEN_GLF)) #Only looking at Shrimp trawl, GEAR_SIZE=40 and MESH_SIZE=1.63
 
 
-TL<- 969*(1-exp(-.192*(t-.02))) #TL relationship from Patterson et al 2001
-TL
-#Length at maturity= 365mm
-#Frequency of Mesh Size and Gear Size
+#What type of lengths are they measuring
+find(length_freq_gs40_ms163$MEASCD_GLF==1)
+
+#Von Bert Size at Age
+len=c(1:5) #dimension variable of typical length at age
+for (t in 1:5){
+  len[t]=856.4*(1-exp(-.19*(t--0.395))) #Von Bert growth, coefficients from Brad's website
+  rm(t)
+}
+
+Lm= 230 #Length at maturity Red Snapper
+        #Maturity obtained at year 2, but estimate for size at year 2 is greater than 230cm
+
+#Frequency distribution of Red Snapper
+
+qplot(length_freq_gs40_ms163$LEN_GLF,
+geom="histogram",
+binwidth = 50,
+main = "Red Snapper Length Frequency",
+xlab = "Length (mm)",
+fill=I("blue"),
+col=I("red"),
+alpha=I(.2),
+xlim=c(0,400))
+data.fram
+
+unique(length_freq_gs40_ms163$MEASCD_GLF)
+
+
+data.frame(matrix(NA,4,1), row.names = c("Fork", "Standard", "Total", "NA"), colnames("n"))
+
+#Questions
+  #Find indices
+  #How to rename axes, add more points to plot, change axes to percent of total in bin
 
 
 
