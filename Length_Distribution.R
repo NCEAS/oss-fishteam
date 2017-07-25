0160f3177585220d40fd8052080f3dfaef6688a9
OSS2017 <ilich@aurora>
2017-07-25 16:12:01 -0700

    Length Frequency plot, % juveniles, % gear type, and % measurement type completed

--- Length_Distribution.R
+++ Length_Distribution.R
@@ -11,24 +11,59 @@ INV<- INVREC %>% select(CRUISEID, STATIONID, INVRECID, GEAR_TYPE, GEAR_SIZE, MES
 
 GLF<- GLFREC %>% select(CRUISEID, STATIONID, GLFID, SPEC_GLF, LEN_GLF, MEASCD_GLF) %>% filter(SPEC_GLF=="CAMPEC") #Remove some columns and filter for red snapper
 
+#Make Sure these are all true!
 length(unique(INV$STATIONID))==length(INV$STATIONID) #Station ID is unique after filtering in tows of INV
-
 length_freq<- left_join(select(GLF, CRUISEID, STATIONID, LEN_GLF, MEASCD_GLF), select(INV, STATIONID, GEAR_TYPE, GEAR_SIZE, MESH_SIZE), by="STATIONID")
-
 length(length_freq$STATIONID)==length(GLF$STATIONID) #New size matches GLF so not repating length values. Merge successful.
 
 #Check freqency of each gear type
 length_freq<- length_freq %>% mutate(GEAR_MERGE=paste(GEAR_TYPE,GEAR_SIZE,MESH_SIZE, sep="_"))
-Gear_Counts<- length_freq %>% count(GEAR_MERGE)
-Percent_ST401.63<- ((filter(Gear_Counts, GEAR_MERGE=="ST_40_1.63") %>% select(n))/length(length_freq$STATIONID)*100)[[1]]
-      # 91.78001     Almost 92% of red snapper catches are with Shrimp trawl of size 40 and mesh size 1.63
+(table(length_freq$GEAR_MERGE)/length(length_freq$GEAR_MERGE))*100 #Table of gear frequency in percent
+# NA_NA_NA  ST_16_0.25   ST_20_1.5  ST_40_1.58  ST_40_1.63  ST_40_1.65 
+# 5.53870931  0.00880758  1.43689369  0.90340602 91.78001183  0.33217157 
+# Almost 92% of red snapper catches are with Shrimp trawl of size 40 and mesh size 1.63
+
 
 #Extract most common gear type and remove ones that are size of NA
 length_freq_gs40_ms163<-length_freq %>% filter(GEAR_SIZE==40, MESH_SIZE==1.63) %>% filter(!is.na(LEN_GLF)) #Only looking at Shrimp trawl, GEAR_SIZE=40 and MESH_SIZE=1.63
 
 
 #What type of lengths are they measuring
-find(length_freq_gs40_ms163$MEASCD_GLF==1)
+meas_type<- data.frame(table(length_freq_gs40_ms163$MEASCD_GLF))
+meas_type<- meas_type %>% mutate(meas= "empty")
+
+meas_type$meas[meas_type$Var1==1]="Fork"
+meas_type$meas[meas_type$Var1==51]="Fork"
+meas_type$meas[meas_type$Var1==2]="Standard"
+meas_type$meas[meas_type$Var1==11]="Total"
+meas_type$meas[meas_type$Var1==18]="Total"
+meas_type$meas[meas_type$Var1==53]="Total"
+meas_freq<- meas_type %>% group_by(meas) %>% summarize(Freq=sum(Freq))
+meas_freq<- meas_freq %>% mutate(percent=(Freq/sum(Freq))*100)
+
+# meas  Freq     percent
+# <chr> <int>       <dbl>
+#   1     Fork 70243 96.32489064
+# 2 Standard    14  0.01919833
+# 3    Total  2666  3.65591103
+
+#Approximately 96% are fork length, but leaving all measurements b/c all used for catch and probably not that different
+
+
+Lm= 230 #Length at maturity Red Snapper
+        #Maturity obtained at year 2, but estimate for size at year 2 is greater than 230cm
+
+#Frequency distribution of Red Snapper
+
+ggplot(length_freq_gs40_ms163, aes(length_freq_gs40_ms163$LEN_GLF))+
+  geom_histogram()+
+  annotate(geom="text",x=235, y=20000, label="Lm", hjust=0)+
+  geom_vline(xintercept=230)+
+  labs(x="Length (mm)", y="counts", title="Length Distribution")+
+  theme_update(plot.title = element_text(hjust = 0.5))
+
+percent_juv= (sum(length_freq_gs40_ms163$LEN_GLF<230)/length(length_freq_gs40_ms163$LEN_GLF))*100
+# 94.30103 are Juveniles
 
 #Von Bert Size at Age
 len=c(1:5) #dimension variable of typical length at age
@@ -36,32 +71,6 @@ for (t in 1:5){
   len[t]=856.4*(1-exp(-.19*(t--0.395))) #Von Bert growth, coefficients from Brad's website
   rm(t)
 }
-
-Lm= 230 #Length at maturity Red Snapper
-        #Maturity obtained at year 2, but estimate for size at year 2 is greater than 230cm
-
-#Frequency distribution of Red Snapper
-
-qplot(length_freq_gs40_ms163$LEN_GLF,
-geom="histogram",
-binwidth = 50,
-main = "Red Snapper Length Frequency",
-xlab = "Length (mm)",
-fill=I("blue"),
-col=I("red"),
-alpha=I(.2),
-xlim=c(0,400))
-data.fram
-
-unique(length_freq_gs40_ms163$MEASCD_GLF)
-
-
-data.frame(matrix(NA,4,1), row.names = c("Fork", "Standard", "Total", "NA"), colnames("n"))
-
-#Questions
-  #Find indices
-  #How to rename axes, add more points to plot, change axes to percent of total in bin
-
 
 
 
