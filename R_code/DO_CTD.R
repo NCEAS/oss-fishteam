986d22847e95c002a4195f6f66f42a7bbee0c18b
OSS2017 <ilich@aurora>
2017-07-27 00:44:57 -0700

    Continued Working on bottom DO

--- R_code/DO_CTD.R
+++ R_code/DO_CTD.R
@@ -1,3 +1,10 @@
+rm(list=ls())
+library(tidyverse)
 setwd("~/oss/Synthesis")
-env<- read.csv("./env_loc_date_depth_all.csv", stringsAsFactors = FALSE, header = TRUE)
-               
+env<- tbl_df(read.csv("./env_loc_date_depth_all.csv", stringsAsFactors = FALSE, header = TRUE))
+env_trim<- env %>% select(X,Cast, Depth, Oxygen, lon, lat, Bottom.depth, year, month)
+length(unique(env_trim$X))==length(env_trim$X)
+#[1] TRUE # X is a unique ID for row
+DO<- env_trim %>% group_by(Cast) %>% summarize(max_depth=max(Depth))
+
+#add X, lat, long, year, month
